a3d4aed580108b32a89d98c50ee6a6e0adcb1f18
abc1127 <oscarmui1127@gmail.com>
2019-05-16 23:25:08 +0800

    Done D100-102 using py

---
+++ HKOI_D/D101.py
@@ -0,0 +1,6 @@
+phone = input("")
+
+if phone[0]=='2' or phone[0]=='3':
+    print("Fixed")
+elif phone[0]=='5' or phone[0]=='6' or phone[0]=='9':
+    print("Mobile")
---
+++ HKOI_D/D102.py
@@ -0,0 +1,3 @@
+string = input("")
+fare = round(float(string[1:])/2.0,1)
+print("$%.1f" % fare)
